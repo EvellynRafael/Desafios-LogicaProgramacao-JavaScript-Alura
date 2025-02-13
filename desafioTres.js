e148d6c9d494521e73dcd686b2765dcf2090a419
Evellyn Rafael <71461331+EvellynRafael@users.noreply.github.com>
2025-02-13 15:52:16 -0300

    Create desafioTres.js
    Exercícios do desafio 3: Loops e Tentativas

---
+++ desafioTres.js
@@ -0,0 +1,28 @@
+//De 1 até 10
+let contador = 1;
+while (contador <= 10){
+    console.log(contador);
+    contador++
+}  
+
+//De 10 até 0
+let contadorDois = 10;
+while (contadorDois >= 0){
+    console.log(contadorDois);
+    contadorDois--;
+} 
+
+//Contagem regressiva
+let numero =  prompt('Digite um número maior do que 0');
+while (numero >= 0){
+    console.log(numero);
+    numero--;
+} 
+
+//Contagem progressiva
+let inicio = 0;
+let contagem = prompt('Digite um número maior do que 0');
+while (inicio <= contagem){
+    console.log(inicio);
+    inicio++;
+}
